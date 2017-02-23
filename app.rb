301e29d072393c0190c6e9b5c72f71fb9191fcd3
[maxximus87] <[maxxturner10@gmail.com]>
2017-02-23 10:45:20 -0500

    major change

--- app.rb
+++ app.rb
@@ -5,16 +5,8 @@ require 'bcrypt'
 require 'pony'
 require 'bcrypt'
 require_relative "grandbash_functions.rb"
-load "./local_env.rb" if File.exists?("./local_env.rb")
 
 
-
-set :sessions,
-    key: ENV['key'],
-     #domain: "grandbash.herokuapp.com",
-    domain: "localhost",
-    path: '/',
-    secret: ENV['secret']
 
 get '/' do
     @title = 'Grandbash 2017'
@@ -79,27 +71,12 @@ post '/winning_number_search' do
 end
  
 
-post '/subscribe' do
-    email= params[:email]
-    check_email = db.exec("SELECT * FROM maillist WHERE email_address = '#{email}'")
-       
-    if check_email.num_tuples.zero? == false
-            erb :mailinglist, :locals => {:message => "You have already joined our mailing list"}
-    else
-         subscribe=db.exec("insert into maillist(email_address)VALUES('#{email}')")
-         erb :mailinglist, :locals => {:message => "Thanks, for joining our mailing list."}
-    end
-end
-    
 get '/contact' do
     @title = 'GrandBash| Contact Us'
     erb :contact
 end
 
-get '/privacy' do
-    @title = 'GrandBash| Refund & Privacy Policy'
-    erb :privacy
-end
+
 
 post '/contact-form' do
   name = params[:name]
@@ -135,140 +112,11 @@ get '/dayofevent' do
     erb :dayofevent, :locals => {:prizes => prizes}
 end
 
-get '/profile' do
-    @title = 'GrandBash| User profile'
-    erb :profile, :locals => {:message => " ", :message1 => " "}
-end
 
-post '/profile' do
-    fname = params[:first_name]
-    lname = params[:last_name]
-    address = params[:address]
-    city = params[:city]
-    state = params[:state]
-    zip = params[:zip]
-    email = params[:email]
-    phone = params[:phone]
-    user_name = params[:user_name]
-    user_pass = params[:user_pass]
-    
-    #This is for creating profile and preventing duplication
-    check_username = db.exec("SELECT * FROM users WHERE user_name = '#{user_name}'")
-    
-    check_email = db.exec("SELECT * FROM users WHERE email = '#{email}'")
-    
-    hash = BCrypt::Password.create(user_pass, :cost => 11) 
-    
-    
-        if check_username.num_tuples.zero? == false
-            erb :profile, :locals => {:message => "That user name already exists", :message1 => " "}
-        elsif 
-            check_email.num_tuples.zero? == false
-            erb :profile, :locals => {:message => " ", :message1 => "That email already exists"}
-        else
-            db.exec ("INSERT INTO users (name, address, city, state, zipcode, email, user_name, encrypted_password) VALUES ('#{fname}  #{lname}', '#{address}','#{city}', '#{state}', '#{zip}', '#{email}', '#{user_name}', '#{hash}'  )" )
-            erb :success, :locals => {:message => "You have successfully created a new profile.", :message1 => " "}
-        end
-end
 
-post '/login' do
-    # Standard Log In
-    
-    user_name = params[:user_name]
-    user_pass = params[:user_pass]
-    email = params[:email]
-    
-    match_login = db.exec("SELECT user_name, encrypted_password, usertype, email FROM users WHERE user_name = '#{user_name}'")
-    
-        if match_login.num_tuples.zero? == true
-            error = erb :login, :locals => {:message => "invalid username and password combination"}
-            return error
-        end
-    
-    password = match_login[0]['encrypted_password']
-    comparePassword = BCrypt::Password.new(password)
-    usertype = match_login[0]['usertype']
-    email =  match_login[0]['email']
-    
-      if match_login[0]['user_name'] == user_name &&  comparePassword == user_pass
 
-      session[:user] = user_name
-      session[:usertype] = usertype
-          session[:email] = email  
-          erb :index, :locals => {:get_text => " "}
-      else
-      erb :login, :locals => {:message => "invalid username and password combination"}
-      end
-    redirect '/' 
-end
 
-get '/edit_profile' do
-    @title = 'GrandBash|Edit Profile' 
-    edit_profile = db.exec("SELECT * FROM users where email = '#{session[:email]}' ")    
-    erb :edit_profile, :locals => {:edit_profile => edit_profile}
-end
 
-post '/edit_profile' do
-   name = params[:name]
-   address = params[:address]
-   city = params[:city]
-   state = params[:state]
-   zip = params[:zip]
-   email = params[:email]
-   phone = params[:phone]
-           
-    update_profile = db.exec ("UPDATE users SET (name, address, city, state, zipcode, email, phone)  =  ('#{name}', '#{address}','#{city}', '#{state}', '#{zip}', '#{email}', '#{phone}' ) WHERE email = '#{email}'" )
-    
-    redirect '/'
-end
-
-get '/success' do
-    @title = 'Success'
-    erb :success
-end
-
-get '/logout' do
-  session[:user] = nil
-  session[:usertype] = nil
-  redirect '/'
-end
-
-post '/facebook' do
-    @title = 'GrandBash| Facebook Login'    
-    name = params[:name]
-    email = params[:email]
-    
-    check_email = db.exec("SELECT * FROM users WHERE email = '#{email}'")
-    
-    if check_email.count > 0
-        puts '#{check_email}'
-    else
-        facebook_log = db.exec ("INSERT INTO users (open_sso_data, name, email) VALUES ('facebook', '#{name}','#{email}' )" )
-    end
-    session[:user] = name
-    session[:email] = email
-    puts "session user: #{session[:user]}"
-    puts "session email: #{session[:email]}"
-    redirect '/'   
-end
-
-post '/google' do
-    @title = 'GrandBash|Google Login'
-    
-    name = params[:gname]
-    email = params[:gemail]
-    
-    check_email = db.exec("SELECT * FROM users WHERE email = '#{email}'")
-    
-    if check_email.count > 0
-        puts '#{check_email}'
-    else
-        google_log = db.exec ("INSERT INTO users (open_sso_data, name, email) VALUES ('google', '#{name}','#{email}' )" )
-    end
-    session[:user] = name
-    session[:email] = email
-    redirect '/'   
-end
 
 get '/admin_page' do
    @title = 'GrandBash| Admin Page'
